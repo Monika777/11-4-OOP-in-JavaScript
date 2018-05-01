01db9f124268695f90ae9cedf571b7b55059ece2
Monika Tarka <monikatarka@yahoo.co.uk>
2018-05-01 15:45:37 +0100

    Modified script,js file

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 
-function Phone(brand, price, color) {
+function Phone(brand, price, color, system) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
+	this.system = system;
 }
 Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is £" + this.price + ".");
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + ", operating system - " + this.system + " and the price is £" + this.price +".");
 }
-var samsungGalaxyS6 = new Phone("Samsung", 650, "gold");
-var iPhone6S = new Phone("Apple", 1250, "silver");
-var onePlusOne = new Phone("OnePlus", 470, "black");
+var samsungGalaxyS6 = new Phone("Samsung", 650, "gold", "Android");
+var iPhone6S = new Phone("Apple", 1250, "silver", "Apple");
+var onePlusOne = new Phone("OnePlus", 470, "black", "Windows");
 
 samsungGalaxyS6.printInfo();
 iPhone6S.printInfo();
